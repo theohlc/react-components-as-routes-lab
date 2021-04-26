a1a7a29d48fa438cf8300e4c3103bab0c67a12fe
theo <teddyhoover@gmail.com>
2021-04-26 11:32:36 -0500

    commit tree 20f877429707fd0be82eea9cea781d9369ac3f57

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,15 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {movies.map(movie => (
+          <div>
+            {movie.title}<br></br>
+            {movie.time}<br></br>
+            {movie.genres.join(", ")}<br></br>
+            {movie.metascore}<br></br>
+          </div>
+        ))}
     </div>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <NavLink
+        to='/'
+        exact
+        style={link}
+      >Home</NavLink>
+      <NavLink
+        to='/movies'
+        exact
+        style={link}
+      >Movies</NavLink>
+      <NavLink
+        to='/directors'
+        exact
+        style={link}
+      >Directors</NavLink>
+      <NavLink
+        to='/actors'
+        exact
+        style={link}
+      >Actors</NavLink>
     </div>
   );
 };
